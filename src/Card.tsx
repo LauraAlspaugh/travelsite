dc6554be519157fa0399f237bbe0f0e2ad79ed29
Laura <lauraalspaugh2002@icloud.com>
2024-05-02 15:04:22 -0400

    finished with project 3!

--- src/Card.tsx
+++ src/Card.tsx
@@ -7,7 +7,7 @@ export default function Card(props){
        <img className="card-image" alt="card" src={props.item.picture}/>
             </div>
         <div className="info-holder">
-       <h3><i className="mdi mdi-map-marker map"></i>{props.item.location}</h3>
+       <h3><i className="mdi mdi-map-marker map"></i>{props.item.location} View on  <a className="text-light" href="https://www.google.com/maps/">Google Maps</a></h3>
        <h1>{props.item.name}</h1>
        <h5>{props.item.date}</h5>
        <p>{props.item.description}</p>
